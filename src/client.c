a59036620a68f7f2cb8e85e41a0229df4be92c29
Sukit Deeyiengyang <kitdee@hotmail.com>
2023-01-27 08:35:34 +0700

    Tried to improve server.c to make it run faster to prevent missing signal

--- src/client.c
+++ src/client.c
@@ -6,30 +6,29 @@
 /*   By: sdeeyien <sukitd@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 14:55:52 by sdeeyien          #+#    #+#             */
-/*   Updated: 2023/01/25 01:09:07 by sdeeyien         ###   ########.fr       */
+/*   Updated: 2023/01/27 08:29:08 by sdeeyien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void send_to_server(pid_t pid, unsigned chr)
+volatile char	*g_argv[2];
+
+static void	send_to_server(pid_t pid, unsigned char chr)
 {
 	int	i;
 
 	i = 0;
 	while (i < BYTE)
 	{
-
 		if (chr & 1)
-		{
 			kill(pid, SIGUSR2);
-			usleep(50);
-		}
 		else
-		{
 			kill(pid, SIGUSR1);
-			usleep(50);
-		}
+		if (i == 7)
+			usleep(30);
+		else
+			usleep(100);
 		chr >>= 1;
 		i++;
 	}
@@ -60,8 +59,30 @@ int	main(int argc, char *argv[])
 		ft_printf("Error\n: Fewer or Larger parameters");
 		return (1);
 	}
+	g_argv[0] = argv[1];
+	g_argv[1] = argv[2];
 	pid_no = ft_atoi(argv[1]);
 	if (str_to_bin(pid_no, argv[2]))
 		ft_printf("Invalid message\n");
 	return (0);
 }
+
+/*
+int	main(int argc, char *argv[])
+{
+	pid_t	pid_no;
+	static unsigned char	bit_mask[] = {1, 1, 1, 1, 1, 1, 1};
+
+	pid_no = 0;
+	if (argc != 3)
+	{
+		ft_printf("Error\n: Fewer or Larger parameters");
+		return (1);
+	}
+	pid_no = ft_atoi(argv[1]);
+	printf("strlen of bit_mask = %lu\n", ft_strlen((char *) bit_mask));
+	if (str_to_bin(pid_no, argv[2]))
+		ft_printf("Invalid message\n");
+	return (0);
+}
+*/
--- src/server.c
+++ src/server.c
@@ -6,16 +6,36 @@
 /*   By: sdeeyien <sukitd@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 14:05:34 by sdeeyien          #+#    #+#             */
-/*   Updated: 2023/01/25 01:38:49 by sdeeyien         ###   ########.fr       */
+/*   Updated: 2023/01/27 08:27:32 by sdeeyien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void bit_to_char(int signum)
+static void	bit_to_char(int signum, siginfo_t *info, void *context)
 {
-	static int bits_count;
-	static unsigned char chr;
+	static int				bits_count;
+	static unsigned char	chr = 0;
+	static unsigned char	bit_shift[] = {1, 1, 1, 1, 1, 1, 1};
+
+	(void) context;
+	(void) info;
+	bits_count++;
+	if (signum == SIGUSR2)
+		chr |= 128;
+	chr >>= bit_shift[bits_count - 1];
+	if (bits_count == 8)
+	{
+		write(1, &chr, 1);
+		bits_count = 0;
+		chr = 0;
+	}
+}
+/*
+void	bit_to_char(int signum)
+{
+	static int				bits_count = 0;
+	static unsigned char	chr = 0;
 
 	bits_count++;
 	if (signum == SIGUSR1 && bits_count != 8)
@@ -33,7 +53,30 @@ void bit_to_char(int signum)
 		chr = 0;
 	}
 }
+*/
 
+int	main(void)
+{
+	int					pid;
+	struct sigaction	s_sigaction;
+
+	ft_memset(&s_sigaction, 0, sizeof(s_sigaction));
+	pid = getpid();
+	ft_putstr_fd("Server PID = ", pid);
+	ft_putnbr_fd(pid, 1);
+	ft_putchar_fd('\n', 1);
+	s_sigaction.sa_sigaction = bit_to_char;
+	s_sigaction.sa_flags = SA_SIGINFO | SA_RESTART | SA_NODEFER;
+	sigaction(SIGUSR1, &s_sigaction, NULL);
+	sigaction(SIGUSR2, &s_sigaction, NULL);
+	while (1)
+	{
+		pause();
+	}
+	return (0);
+}
+
+/*
 int	main(void)
 {
 	ft_printf("Server PID = %d\n", getpid());
@@ -45,3 +88,4 @@ int	main(void)
 	}
 	return (0);
 }
+*/
